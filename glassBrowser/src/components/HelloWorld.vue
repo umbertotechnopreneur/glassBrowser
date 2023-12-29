e85ad0c81b50cc2a323f8ded2bb5f3e2824b85f7
Umberto Giacobbi <umberto.giacobbi@gmail.com>
2023-12-29 19:05:58 +0700

    Added few 3d options there still some work to do on the shadow of the browser, if someone helps. For now the development stops here for us.

--- glassBrowser/src/components/HelloWorld.vue
+++ glassBrowser/src/components/HelloWorld.vue
@@ -9,40 +9,49 @@
 <template>
   <v-container :style="backgroundStyle" fluid class=" d-flex justify-center align-center">
     <!-- This is our outer window, the background changes dynamically at each refresh-->
-    <section class="browser" ref="browserSection">
-
+    <section class="browser thumb" ref="browserSection" @mousemove="handleMouseMove">
       <v-container v-show="showTools" class="adminBar">
         <div class="title">Admin area</div>
-
         <v-row>
           <v-col cols="12">
-            <p>Hey pro tip: press the 'h' key anytime to show/hide the admin panel, so you can get your gorgeus screenshot
-              without any stupid stuff in it.<br />
-              Refresh the browser window to change the backgound image of the page.
-              Drop the image on the designed area or the 'browser document' to change it.</p>
-
+            <p>Hey some pro tip:</p>
+            <ul class="ml-8">
+              <li>Press <b>h</b> to hide/show the admin area</li>
+              <li>Press <b>s</b> to enable/disable 3d manipulation</li>
+              <li>Press <b>r</b> to reset the perspective</li>
+              <li>Press <b>n or m</b> will decrease or increase the scaling by 0.01 <b>b</b> will reset to 1</li>
+              <li>Refresh the browser window to change the backgound image of the page.</li>
+              <li>Drop the image on the designed area or the 'browser document' to chane it.</li>
+            </ul>
           </v-col>
           <v-col cols="12" md="4">
-            <v-text-field v-model="browserTitle" clearable hide-details="auto" label="Page title"></v-text-field>
+            <v-text-field v-model="browserTitle" clearable hide-details label="Page title"></v-text-field>
           </v-col>
           <v-col cols="12" md="8" class="d-flex align-center">
-            <v-text-field v-model="imageUrl" clearable hide-details="auto"
-              label="Image url (local or whatever)"></v-text-field>
+            <v-text-field v-model="imageUrl" clearable hide-details label="Image url (local or whatever)"></v-text-field>
             <v-btn class="mx-4" @drop.prevent="onFileDrop" @dragover.prevent>drop the screenshot here</v-btn>
           </v-col>
-          <v-col cols="12">
-            <v-textarea clearable label="Current browser CSS style" style="font-family: monospace;">
-            sdsadf
-            </v-textarea>
+          <v-col cols="12" class="d-flex align-center justify-center">
+            <v-switch hide-details label="Switch 3D manipulation via mouse on and off"
+              v-model="enable3DManipulation"></v-switch>
+            <v-text-field hide-details label="Current scale" class="mx-4" v-model="actualScaling"></v-text-field>
+            <v-slider hide-details v-model="actualScaling" max="1.5" min="0.5" step="0.01">
+            </v-slider>
+            <v-chip class="mx-4" v-if="enable3DManipulation">3D manipulation
+              enabled</v-chip>
+            <v-chip class="mx-4" v-if="!enable3DManipulation">3D manipulation
+              NOT enabled</v-chip>
+
+            <v-btn size="small" @click="resetPerspective">reset perspective</v-btn>
           </v-col>
         </v-row>
 
-        <p class="text-caption"><b>Help wanted:</b> For <a href="https://aginti.it">aginti.it</a> (and other porjects) we
+        <p class="text-caption"><b>Help wanted:</b> For <a href="https://aginti.it">aginti.it</a> (and other projects) we
           use this tool extensively. If you have time consider to add some features to it. Thanks for your contribution!
         </p>
 
-        <p class="text-caption">For example what about 3d <a
-            href="https://polypane.app/css-3d-transform-examples">here</a> you can get some inspiration.</p>
+        <p class="text-caption">For example what about a better 3d handling? <a
+            href="https://polypane.app/css-3d-transform-examples">Here</a> you can get some inspiration.</p>
       </v-container>
 
       <!-- === Title bar === -->
@@ -91,12 +100,84 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 // Add here your variables values
 const browserTitle = ref('Default browser title');
+
+const scaleTransform = ref('');
+const rotationTransform = ref('');
+
 const showTools = ref(true);
+const hovered = ref(true);
+const enable3DManipulation = ref(false);
 const imageUrl = ref('../../images/yourScreenshot.jpg');
+const actualScaling = ref(1);
+
+watch(actualScaling, (newValue, oldValue) => {
+  // This function will run when actualScaling changes
+  console.log(`actualScaling changed from ${oldValue} to ${newValue}`);
+
+  scaleTransform.value = 'scale(' + newValue + ')';
+
+  applyTransformations();
+});
+
+function applyTransformations() {
+  const thumb = document.getElementsByClassName('thumb')[0];
+
+  thumb.style.transform = scaleTransform.value + ' ' + rotationTransform.value;
+}
+
+// Got inspiration from https://codepen.io/MarioDesigns/pen/NdygoP
+const handleMouseMove = (event) => {
+  if (!enable3DManipulation.value)
+    return;
+
+  // Your mousemove logic here
+  console.log(`Mouse moved at ${event.clientX}, ${event.clientY}`);
+
+  //set the hovered var to true
+  hovered.value = true;
+
+  const thumb = document.getElementsByClassName('thumb')[0];
+
+  const classes = thumb.classList;
+
+  // Log the classes
+  for (let i = 0; i < classes.length; i++) {
+    console.log(classes[i]);
+  }
+
+  //on hover if thumb has class idle removes it
+  if (classes.contains('idle')) {
+    classes.remove('idle');
+  }
+
+  // Define vars
+  //gets the half point horizontally
+  var hCenter = thumb.offsetWidth / 2;
+  //gets the half point vertically
+  var vCenter = thumb.offsetHeight / 2;
+  //gets the x coord of the pointer
+  var relativeX = (event.pageX - thumb.offsetLeft);
+  //gets the y coords of the pointer
+  var relativeY = (event.pageY - thumb.offsetTop);
+  //calculates the x rotation
+  var xRotation = (relativeY - vCenter) * 0.1;
+  //calculates the y rotation
+  var yRotation = (relativeX - hCenter) * 0.05;
+
+  //Apply rotation to the element
+  rotationTransform.value = 'rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)';
+
+  applyTransformations();
+};
+
+function resetPerspective() {
+  const thumb = document.getElementsByClassName('thumb')[0];
+  thumb.style.transform = 'rotatex(0deg) rotatey(0deg)';
+}
 
 const randomNumber = ref(Math.floor(Math.random() * 17) + 1);
 
@@ -132,21 +213,52 @@ function onFileDrop(e) {
 import { onMounted } from 'vue';
 
 onMounted(async () => {
-
   document.addEventListener('keyup', function (event) {
     if (event.key === 'h') {
       console.log('The "h" key was pressed up.');
 
       showTools.value = !showTools.value;
     }
+
+    if (event.key === 's') {
+      console.log('The "s" key was pressed up.');
+
+      enable3DManipulation.value = !enable3DManipulation.value;
+    }
+
+    if (event.key === 'r') {
+      console.log('The "r" key was pressed up.');
+
+      enable3DManipulation.value = false;
+      resetPerspective();
+    }
+
+    if (event.key === 'n') {
+      console.log('The "n" key was pressed up.');
+
+      actualScaling.value = actualScaling.value - 0.01;
+    }
+
+    if (event.key === 'm') {
+      console.log('The "m" key was pressed up.');
+
+      actualScaling.value = actualScaling.value + 0.01;
+    }
+
+    if (event.key === 'b') {
+      console.log('The "b" key was pressed up.');
+
+      actualScaling.value = 1;
+    }
+
   })
+
 
 });
 </script>
 
 <style scoped>
 /* === Browser overall layout  === */
-
 .browser {
   background-image: linear-gradient(to bottom right,
       rgba(255, 255, 255, 0.3) 0%,
@@ -180,6 +292,112 @@ onMounted(async () => {
     pointer-events: none;
   }
 }
+
+/* === Browser 3d layout  === */
+
+.thumb {
+  transform-style: preserve-3d;
+  transform-origin: center;
+  transition: all 0.2s linear;
+  transform: rotatex(0deg) rotatey(0deg);
+
+  &.idle {
+    &:after {
+      transform: translatez(25px) translatey(0px);
+      opacity: 1;
+    }
+
+    &:before {
+      transform: translatez(25px) translatey(0px);
+      opacity: 1;
+    }
+  }
+
+  &:after {
+    content: 'Hover Me';
+    font-size: 12px;
+    color: silver;
+    position: absolute;
+    top: -55px;
+    left: 50%;
+    width: 80px;
+    margin-left: -40px;
+    background-color: rgba(255, 255, 255, 0.4);
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 3px;
+    transform: translatez(25px) translatey(20px);
+    transition: all 0.4s ease-in-out;
+    opacity: 0;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: -32px;
+    left: 50%;
+    margin-left: -10px;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 8px 10px 0 10px;
+    border-color: rgba(255, 255, 255, 0.4) transparent transparent transparent;
+    transform: translatez(25px) translatey(20px);
+    transition: all 0.4s ease-in-out;
+    opacity: 0;
+  }
+
+  .backlight {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 320px;
+    height: 180px;
+    background-color: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 0px 100px 40px rgba(255, 255, 255, 0.2);
+    transform: translateZ(-40px);
+    z-index: 1;
+  }
+
+  .player {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 320px;
+    height: 180px;
+    opacity: 1;
+    border-radius: 5px;
+    background-color: black;
+    background-image: url('https://raw.githubusercontent.com/Mario-Duarte/CodePen/main/assets/alien-world.jpg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    transform: translateZ(20px);
+    z-index: 2;
+  }
+
+  .shine {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 320px;
+    height: 180px;
+    border-radius: 5px;
+    /*
+    background-color: rgba(255,255,255,1);
+		background: radial-gradient(ellipse at 50% 50%, rgba(255,255,255,0.6) 0%,rgba(51,51,51,0) 100%);
+    */
+    transform: translateZ(20px);
+    overflow: hidden;
+    pointer-events: none;
+    z-index: 3;
+  }
+}
+
+/* === End Browser 3d layout  === */
 
 .title {
   font-size: 1.1rem;
@@ -289,7 +507,7 @@ onMounted(async () => {
   height: 25px;
   display: flex;
   justify-content: center;
-  align-items: center; 
+  align-items: center;
 }
 
 .titleBarActionButtonsContainer .button a {
